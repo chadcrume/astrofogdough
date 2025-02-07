64a507e4f0f18d7e6005cb6815f55aa83854a669
CCC <chad.crume@gmail.com>
2025-02-07 08:07:14 -0500

    A minimal start to using PrimeVue

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,6 +1,13 @@
 import type { App } from 'vue';
 import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+// import Ripple from 'primevue/ripple';
 
 export default (app: App) => {
-    app.use(PrimeVue);
+    // app.directive('ripple', Ripple);
+    app.use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    });
 };
